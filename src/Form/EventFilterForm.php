3897746824bb071e2f79501045c9f36eb57f1899
Jim Safley <jimsafley@gmail.com>
2025-05-14 09:22:01 -0400

    Remove first filter group so first element can be focused

--- src/Form/EventFilterForm.php
+++ src/Form/EventFilterForm.php
@@ -16,7 +16,7 @@ class EventFilterForm extends Form
             'type' => LaminasElement\Text::class,
             'name' => 'id',
             'options' => [
-                'label' => 'Event ID', // @translate
+                'label' => 'ID', // @translate
             ],
             'attributes' => [
                 'placeholder' => 'Enter an ID', // @translate
@@ -26,7 +26,7 @@ class EventFilterForm extends Form
             'type' => LaminasElement\Select::class,
             'name' => 'event',
             'options' => [
-                'label' => 'Event name',
+                'label' => 'Event',
                 'empty_option' => 'Select an event', // @translate
                 'value_options' => $this->getEventValueOptions(),
             ],
@@ -35,7 +35,7 @@ class EventFilterForm extends Form
             'type' => LaminasElement\Select::class,
             'name' => 'resource',
             'options' => [
-                'label' => 'Resource name', // @translate
+                'label' => 'Resource', // @translate
                 'empty_option' => 'Select a resource', // @translate
                 'value_options' => $this->getResourceValueOptions(),
             ],
